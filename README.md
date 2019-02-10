30a80f21207d2c8ac7ad796deac69fd1aa76c1be
zhanzr <zhanzr@foxmail.com>
2019-02-10 17:40:28 +0800

    maintaince

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ LCD1602_t1:
 LCD1602_I2C:
 	LCD1602 with PCF8574AT IO Expander driver.
 
+Board		I2C / TWI pins
+Uno, Ethernet	A4 (SDA), A5 (SCL)
+Mega2560	20 (SDA), 21 (SCL)
+Leonardo	2 (SDA), 3 (SCL)
+Due		20 (SDA), 21 (SCL), SDA1, SCL1
--- lcd1602_i2c_simple/lcd1602_i2c_simple.ino
+++ lcd1602_i2c_simple/lcd1602_i2c_simple.ino
@@ -4,26 +4,27 @@
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C  lcd(0x3f,2,1,0,4,5,6,7);
+LiquidCrystal_I2C  lcd(0x3f,2,1,0,4,5,6,7,3,POSITIVE);
 
 void setup()
 {
   // activate LCD module
   lcd.begin (16,2); // for 16 x 2 LCD module
 
-  lcd.setBacklightPin(3, POSITIVE);
   lcd.setBacklight(HIGH);  
 }
 
 void loop()
 {
   lcd.home (); // set cursor to 0,0
-  lcd.print(__TIME__); 
+  lcd.print(__DATE__); 
   lcd.setCursor (0, 1);        // go to start of 2nd line
   lcd.print(millis());
-  delay(1000);
+  delay(2000);
   //lcd.setBacklight(LOW);      // Backlight off
   delay(250);
   //lcd.setBacklight(HIGH);     // Backlight on
-  delay(1000);
+  lcd.print(ARDUINO);
+  delay(2000);
+  
 }
